7f74acf6505fb6a577b03886d34fd1efe1a37ccc
michiqueli <michiqueli@gmail.com>
2023-09-08 11:18:50 -0300

    asd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,36 +5,28 @@ import Error from "./pages/Error/Error.jsx"
 import Details from './pages/Details/Details.jsx'
 import Top from './pages/Top/Top'
 import About from './pages/About/About'
-import Nav from './components/Nav/Nav'
 
-import {useNavigate, Route, Routes} from 'react-router-dom'
-import {useLocation} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 
 
 function App() {
-    const [characters, setCharacters] = useState([]);
-    const [access, setAccess] = useState(false);
-    const location = useLocation();
-    const navigate = useNavigate();
- 
-    function aceptTerms() {
-       /*const URL = 'http://localhost:3001/';
-       axios(URL).then(({ data }) => {
-          const { access } = data;*/
-          setAccess(true);
-          access && navigate('/home')
-       }
+   const [characters, setCharacters] = useState([]);
+   
+   const getAllDrivers = () => {
+      axios(`http://localhost:3001/drivers`)
+      .then((response) => {
+         setCharacters(response.data)
+      })
+   }
 
-    useEffect (() => {
-       !access && navigate('/');
-       },[access, navigate]);
-    
-    const onSearch = (id) =>{
-       axios(`http://localhost:3001/drivers/${id}`).then(({ data }) => {
+
+   const onSearch = (id) =>{
+       axios(`http://localhost:3001/drivers/${id}`)
+       .then(({ data }) => {
           if (data.name) {
-             setCharacters((characters) => [...characters, data]);
+             setCharacters(data);
           } else {
              window.alert('¡No hay personajes con este ID!');
           }
@@ -43,17 +35,15 @@ function App() {
 
   return (
   <div className='App'>
-      {location.pathname !== '/' && <Nav onSearch = {onSearch}/>}
       <Routes>
-        <Route path='/' element={<Landing aceptTerms = {aceptTerms}/>}/>
+        <Route path='/' element={<Landing />}/>
         <Route path='*' element={<Error/>}/>
-        <Route path='/home' element={<Home characters={characters} />}/>
+        <Route path='/home' element={<Home characters={characters} onSearch={onSearch} />}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/top' element={<Top/>}/>
         <Route path='/details/:id' element={<Details/>}/>
       </Routes>
-
-  </div>
+   </div>
   
   )
  
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -9,7 +9,7 @@ export default function Nav ({onSearch}){
         <div className={style.container}>
         <Link to ='./About'><button className={style.about} >About</button></Link>
         <Link to ='./Home'><button className={style.home}>Home</button></Link>
-        <Link to ='./TopTen'><button className={style.home}>Top Ten</button></Link>
+        <Link to ='./Top'><button className={style.home}>Top Ten</button></Link>
         <SearchBar onSearch = {onSearch}></SearchBar>
         </div>
         </div>
