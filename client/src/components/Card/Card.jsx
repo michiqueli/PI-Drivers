16dcdb41d709939dfdd1500faa621cd0ce59a8a4
michqiueli <michiqueli@gmail.com>
2023-09-11 17:17:46 -0300

    mas estilos

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,13 @@ import style from "./Card.module.css";
 import { Link } from 'react-router-dom'
 
 export default function Card({name, lastName, image, id, teams}) {
-
+   const teamArreglo = teams.split(",").join(", ")
    
    return (
       <div className={style.container} style={{ backgroundImage: `url(${image}`}}>
          <Link to = {`/detail/${id}`}>
             <div className={style.name}>{name} {lastName}</div>
-            <div className={style.teams}>Teams: {teams}</div>
+            <div className={style.teams}>Teams: {teamArreglo}</div>
          </Link>
       </div>
    );
