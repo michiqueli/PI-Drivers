61255062ebc91aa10e65713114678fc8aec379de
michqiueli <michiqueli@gmail.com>
2023-09-12 21:42:54 -0300

    Top, Calendar Done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,10 +2,10 @@ import style from './Home.module.css'
 import Nav from "../../components/Nav/Nav"
 import CardsContainer from "../../components/CardsContainer/CardsContainer"
 
-const Home = (characters, onSearch, getAllDrivers) => {
+const Home = (drivers, onSearch, getAllDrivers) => {
     return (
       <div className={style.container}>
-        <CardsContainer className={style.cardCont}character={characters}getAllDrivers={getAllDrivers}/>
+        <CardsContainer className={style.cardCont}character={drivers}getAllDrivers={getAllDrivers}/>
         <Nav className={style.nav} onSearch={onSearch} getAllDrivers={getAllDrivers}/>
       </div>
     )
--- client/src/pages/Top/Top.jsx
+++ client/src/pages/Top/Top.jsx
@@ -1,16 +1,12 @@
 import style from './Top.module.css'
 const Top = () => {
     return (
-      <div className={style.container}>
-        <div className={style.drivers}>
-          <p className= {style.titulo}>"TOP TEN CHAMPIONS DRIVERS"</p>
-          <img className= {style.imagedrv}></img>
+       <div className={style.container}>
+        <div className= {style.titulo}>TOP TEN CHAMPIONS DRIVERS</div>
+        <div className= {style.titulo}>TOP TEN CHAMPIONS TEAMS</div>
+        <div className= {style.imagedrv}></div>
+        <div className= {style.imageteams}></div>
         </div>
-        <div className={style.teams}>
-          <p className= {style.titulo}>"TOP TEN CHAMPIONS TEAMS"</p>
-          <img className= {style.imageteams}></img>
-        </div>
-      </div>
     )
   }
   export default Top
